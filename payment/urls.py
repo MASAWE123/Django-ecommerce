8f599ba90c57e6a81ca1038f693c45200ef8b54c
larson masawe <larsonmasawe8@gmail.com>
2026-06-30 18:51:16 +0300

    update the payment_sucess url

--- payment/urls.py
+++ payment/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('payment_success',views.payment_success,name = 'payment'),
+    path('payment_success',views.payment_success,name = 'payment_sucess'),
     path('payment_failed',views.payment_failed,name = 'payment_failed'),
     path('checkout',views.checkout,name = 'checkout'),
     path('billing_info',views.billing_info,name = 'billing_info'),
