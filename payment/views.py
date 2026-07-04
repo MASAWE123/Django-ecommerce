88c380e6402427522eeb5f08cea1b1d999a417d7
larson masawe <larsonmasawe8@gmail.com>
2026-07-04 13:26:27 +0300

    correct the intasend error

--- payment/views.py
+++ payment/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from store.models import Product
 import datetime
+from intasend import APIService
 
 #import some paypal stuff
 from django.urls import reverse
