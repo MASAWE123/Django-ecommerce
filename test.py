57066a378a1ede0af9cb9aee8ac8da91a27aa77e
larson masawe <larsonmasawe8@gmail.com>
2026-08-12 15:29:40 +0300

    change s3 bucket

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ django.setup()
 ZETTATEL_USERID=os.environ.get('ZETTATEL_USERID')
 ZETTATEL_PASSWORD =os.environ.get('ZETTATEL_PASSWORD')
 ZETTATEL_SENDERID=os.environ.get('ZETTATEL_SENDERID')
-phone = ['254707008119']
+phone = ['254758751538']
 message = "you order have being received"
 def send_sms():
     url = "https://portal.zettatel.com/SMSApi/send"
